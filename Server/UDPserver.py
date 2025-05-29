4e89051767ae06e1db7fc00f9128705a7f7a3f1d
Nancy <nancy050628@qq.com>
2025-05-29 21:15:03 +0800

    create the random port for data transfer

--- Server/UDPserver.py
+++ Server/UDPserver.py
@@ -8,3 +8,5 @@ class FileServerThread(threading.Thread):
     def __init__(self, filename, client_addr, server_port):
         threading.Thread.__init__(self)
         self.filename = filename
+        self.client_addr = client_addr
+        self.port = random.randint(50000, 51000)
