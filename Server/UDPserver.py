3911636a71f1f2345c0a69c50780c1307dcbc028
Nancy <nancy050628@qq.com>
2025-05-29 21:13:54 +0800

    create the name of the file to transfer

--- Server/UDPserver.py
+++ Server/UDPserver.py
@@ -7,3 +7,4 @@ import base64
 class FileServerThread(threading.Thread):
     def __init__(self, filename, client_addr, server_port):
         threading.Thread.__init__(self)
+        self.filename = filename
