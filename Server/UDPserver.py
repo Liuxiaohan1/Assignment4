340803dea8045c01df28b7d68a0aac9aff42ef2e
Nancy <nancy050628@qq.com>
2025-05-29 21:12:39 +0800

    # Initialize the parent Thread class

--- Server/UDPserver.py
+++ Server/UDPserver.py
@@ -3,3 +3,7 @@ import threading
 import os
 import random
 import base64
+
+class FileServerThread(threading.Thread):
+    def __init__(self, filename, client_addr, server_port):
+        threading.Thread.__init__(self)
